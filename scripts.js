a00379777c13a5f4f8970293811e41eae06829b8
Sad Onion <cebulasmutna@gmail.com>
2020-07-27 18:30:04 +0200

    data

--- scripts.js
+++ scripts.js
@@ -9,15 +9,14 @@ function start(){
     $("#plant").html(plant1+pot);
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.status == 200 && xmlhttp.status == 0){
+        
+        
         
         data = JSON.parse(xmlhttp.responseText);
-  }
-  
-};
-xmlhttp.open("GET","plantsData.json",true);
-xmlhttp.send();
-alert(xmlhttp.responseText);
+    };
+    xmlhttp.open("GET","plantsData.json",true);
+    xmlhttp.send();
+
 
 }
 function changePlant(){
