f9903a99c6be920ffaad5d44eb4aff0d86518713
Sad Onion <cebulasmutna@gmail.com>
2020-07-27 19:42:16 +0200

    data

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
 window.onload = start;
 
 var plant1 = "<a onClick='changePlant()'> ___________</a>";
-var plant2 = " ____...____";
 var pot = "\n[___________]\n |         |\n |         |\n |         |\n |_________|";
-var xd = false;
 var data = null;
+var tickNum=0;
 function start(){
     $("#plant").html(plant1+pot);
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
-        
-        
-        
         data = JSON.parse(xmlhttp.responseText);
     };
     xmlhttp.open("GET","plantsData.json",true);
@@ -20,7 +16,12 @@ function start(){
 
 }
 function changePlant(){
-        $("#plant").html(plant2+pot);
+        $("#plant").html(data.grow1+pot);
         
        alert(data.grow1);
+}
+function tick(){
+    $("#plant").html(data.grow[tickNum]+pot);
+    tickNum++;
+    setTimeout(tick, 5000);
 }
