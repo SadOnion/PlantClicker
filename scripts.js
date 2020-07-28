99b4adda73df8da801d7a70a3614b78f14452921
Sad Onion <cebulasmutna@gmail.com>
2020-07-28 12:15:11 +0200

    data

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 window.onload = start;
 
-var plant1 = "<a onClick='tick()'> ___________</a>";
+var plant1 = "<a onClick='plant()'> ___________</a>";
 var pot = "\n[___________]\n |         |\n |         |\n |         |\n |_________|";
 var data = null;
-var maturityLevel=0;
+var maturityLevel=-1;
 var maturity=0;
+var planted = false;
 function start(){
     $("#plant").html(plant1+pot);
     var xmlhttp = new XMLHttpRequest();
@@ -14,17 +15,42 @@ function start(){
     xmlhttp.open("GET","plantsData.json",true);
     xmlhttp.send();
 }
+function autosave(){
+    window.localStorage.setItem("maturityLevel", maturityLevel);
+    window.localStorage.setItem("maturity", maturity);
+    setTimeout(autosave, 60*1000);
+}
+function plant(){
+    $("#plant").html(data.grow[0]+pot);
+    planted = true;
+    window.localStorage.setItem("planted", planted);
+    tick();
+}
 function changePlant(growNum){
+    if(growNum == data.grow.length){
+        $("#plant").html("<a onClick='harvest()'>"+data.grow[maturityLevel]+pot+"</a>");
+    }else{
+
         $("#plant").html(data.grow[maturityLevel]+pot);
+    }
+}
+
+function harvest(){
+    $("#plant").html(plant1+pot);
+    window.localStorage.setItem("maturityLevel", 0);
+    window.localStorage.setItem("maturity", 0);
+    window.localStorage.setItem("planted", false);
+    planted = false;
 }
 function tick(){
-   
     maturity++;
-    var level = Math.floor(maturity / 20);
-    alert(level);
+    var level = Math.floor(maturity / (100/data.grow.length));
     if(level > maturityLevel){
         maturityLevel = level;
         changePlant(maturityLevel);
     }
-    setTimeout(tick, Math.random()*2*1000);
+    if(maturity >= 100){
+        return;
+    }
+    setTimeout(tick, Math.random()*10*1000);
 }
